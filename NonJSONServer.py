f8d07947c0b638c3874ca46d664860423ad69f0e
Krelle156 <kotofteng@gmail.com>
2025-09-20 09:59:02 +0200

    Finished up the server. Moving on the the Client so I can test it.

--- NonJSONServer.py
+++ NonJSONServer.py
@@ -1,4 +1,4 @@
-from threading import *
+from threading import Thread
 from socket import *
 from ServerServiceFunctions import *
 
@@ -9,13 +9,35 @@ serverSocket.listen(2)
 
 def serviceClient(connectionSocketLocal):
     while True:
+
         message = connectionSocketLocal.recv(1024).decode()
 
+        # Nu er det nok ikke en super god praksis, men siden at opaven ikke efterspørger fejlhåndtering kan jeg lige så godt antage at det altid er random/add/subtract
+        # Og at respons så derfor altid bør være "input numbers"
+        inputRequest = 'input numbers'
+        connectionSocketLocal.send(inputRequest.encode())
+        response = ''
 
-    
+        if message.lower() == 'random':
+            clientInputMsg = connectionSocketLocal.recv(1024).decode()
+            numbers = clientInputMsg.split(' ')
+            response = randomInRange(int(numbers[0]), int(numbers[1]))
+        elif message.lower() == 'add':
+            clientInputMsg = connectionSocketLocal.recv(1024).decode()
+            numbers = clientInputMsg.split(' ')
+            response = Add(int(numbers[0]), int(numbers[1]))
+        elif message.lower() == 'subtract':
+            clientInputMsg = connectionSocketLocal.recv(1024).decode()
+            numbers = clientInputMsg.split(' ')
+            response = Subtract(int(numbers[0]), int(numbers[1]))
 
+        connectionSocketLocal.send(str(response).encode())
+
+
+print('The server is listening on port', serverport)
 while True:
-    print('The server is listening on port', serverport)
     clientSocket, addr = serverSocket.accept()
     print('Client, ', addr, 'connected')
+    Thread(target=serviceClient, args=(clientSocket,)).start()
+
     
